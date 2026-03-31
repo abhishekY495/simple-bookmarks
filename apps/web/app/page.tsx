9617465a8d0487e991c7d849faacb3fb5b8f2d97
Abhishek <abhishekY495.sec@gmail.com>
2026-03-31 17:17:24 +0530

    chore: check type build

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,19 @@
+import { AuthUserResponse } from "@repo/schemas";
+
 export default function Home() {
+  const user: AuthUserResponse = {
+    id: "123",
+    email: "asd",
+    fullName: "asd",
+    accessToken: "asd",
+  };
+
   return (
     <div>
       <h1>Simple Bookmarks</h1>
+      <p>{user.id}</p>
+      <p>{user.fullName}</p>
+      <p>{user.email}</p>
     </div>
   );
 }
