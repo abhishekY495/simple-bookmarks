322ec5b5056a9971cae76731f7446b3deb2a51ff
Abhishek <abhishekY495.sec@gmail.com>
2026-03-31 18:14:56 +0530

    feat: Integrate Zod validation and serialization into the main application module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,14 +1,37 @@
-import { Module } from '@nestjs/common';
+import { BadRequestException, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { createZodValidationPipe, ZodSerializerInterceptor } from 'nestjs-zod';
+import { ZodError } from 'zod';
+import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+
+const CustomZodValidationPipe = createZodValidationPipe({
+  createValidationException: (error: unknown) => {
+    const firstErrorMessage =
+      error instanceof ZodError
+        ? (error.issues[0]?.message ?? 'Validation failed')
+        : 'Validation failed';
+    return new BadRequestException(firstErrorMessage);
+  },
+});
 
 @Module({
   imports: [ConfigModule.forRoot(), PrismaModule, AuthModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: CustomZodValidationPipe,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ZodSerializerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
