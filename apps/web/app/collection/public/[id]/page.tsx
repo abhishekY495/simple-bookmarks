bb2242eec7e8238aa4b6b2494faf4f13f157c8d8
Abhishek <abhishekY495.sec@gmail.com>
2026-04-15 17:21:45 +0530

    refactor: add header

--- apps/web/app/collection/public/[id]/page.tsx
+++ apps/web/app/collection/public/[id]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Header } from "@/components/home/header";
 import { PublicBookmark } from "@/components/my/bookmark/public-bookmark";
 import { Spinner } from "@/components/ui/spinner";
 import { getPublicCollectionByIdService } from "@/services/collection-service";
@@ -59,41 +60,44 @@ export default function PublicCollectionPage() {
   }
 
   return (
-    <div className="max-w-6xl mx-auto flex flex-col pb-40">
-      <header className="sticky top-0 z-10 flex flex-col items-center justify-center border-b py-5 pt-4 bg-background">
-        <div className="flex flex-col items-center justify-center gap-1.5">
-          <p className="text-8xl rounded-full">{collection?.emoji}</p>
-          <p className="text-4xl font-semibold">{collection?.name}</p>
-        </div>
-        <div className="flex gap-2 text-muted-foreground text-[15px]">
-          <p>
-            {collection?.bookmarksCount}{" "}
-            {collection?.bookmarksCount && collection?.bookmarksCount > 1
-              ? "bookmarks"
-              : "bookmark"}
-          </p>
-        </div>
-      </header>
-      {/*  */}
-      <div className="p-6">
-        {bookmarks.length > 0 ? (
-          <div className="grid gap-5 grid-cols-2 sm:grid-cols-3">
-            {bookmarks.map((bookmark) => (
-              <PublicBookmark key={bookmark.id} bookmark={bookmark} />
-            ))}
+    <>
+      <Header />
+      <div className="max-w-6xl mx-auto flex flex-col pb-40">
+        <div className="sticky top-0 z-10 flex flex-col items-center justify-center border-b py-5 pt-4 bg-background">
+          <div className="flex flex-col items-center justify-center gap-1.5">
+            <p className="text-8xl rounded-full">{collection?.emoji}</p>
+            <p className="text-4xl font-semibold">{collection?.name}</p>
           </div>
-        ) : (
-          <div className="flex flex-col items-center justify-center gap-2 text-center mt-10">
-            <Image
-              src="/empty-inbox-icon.png"
-              alt="No bookmarks"
-              width={62}
-              height={62}
-            />
-            <p className="font-semibold">No bookmarks</p>
+          <div className="flex gap-2 text-muted-foreground text-[15px]">
+            <p>
+              {collection?.bookmarksCount}{" "}
+              {collection?.bookmarksCount && collection?.bookmarksCount > 1
+                ? "bookmarks"
+                : "bookmark"}
+            </p>
           </div>
-        )}
+        </div>
+        {/*  */}
+        <div className="p-6">
+          {bookmarks.length > 0 ? (
+            <div className="grid gap-5 grid-cols-2 sm:grid-cols-3">
+              {bookmarks.map((bookmark) => (
+                <PublicBookmark key={bookmark.id} bookmark={bookmark} />
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-col items-center justify-center gap-2 text-center mt-10">
+              <Image
+                src="/empty-inbox-icon.png"
+                alt="No bookmarks"
+                width={62}
+                height={62}
+              />
+              <p className="font-semibold">No bookmarks</p>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
